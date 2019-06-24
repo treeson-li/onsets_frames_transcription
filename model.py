1dfc9454721a935477c6fd0308b1e496b86f4566
treeson-li <treeson@gmail.com>
2019-06-24 18:54:30 +0800

    tf.get_variable

--- model.py
+++ model.py
@@ -178,6 +178,7 @@ def attention_method(enc_hidden, enc_output, inputs, labels, num_units, batch_si
     t = tf.Variable(tf.constant(0), dtype=tf.int32, name='decoder_t')
     dec_hidden = enc_hidden
     decoder = attention.Decoder(num_units, batch_size, att_frames)
+    outputs = tf.get_variable("decoder_output", dtype=tf.float32)
     t, outputs, _ = tf.while_loop(condition, body, loop_vars=[t, outputs, dec_hidden], 
                                   shape_invariants=[t.get_shape(), tf.TensorShape([None, batch_size, num_units]), dec_hidden.get_shape()])
 
