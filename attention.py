6f9fc4740bb435fb9d0e0c97f87f82aa7702f524
treeson-li <treeson@gmail.com>
2019-06-24 19:35:03 +0800

    self.units

--- attention.py
+++ attention.py
@@ -91,7 +91,7 @@ class BahdanauAttention(tf.keras.Model):
             return  i, att_value, xpos
 
         zero = lambda: tf.constant(0, dtype=tf.int32)
-        zeros= lambda: tf.zeros([self.att_len, 1, units], dtype=tf.float32)
+        zeros= lambda: tf.zeros([self.att_len, 1, self.units], dtype=tf.float32)
         i = tf.Variable(initial_value=zero, dtype=tf.int32)
         att_value = tf.Variable(initial_value=zeros, dtype=tf.float32)
         _, att_value, xpos = tf.while_loop(cond, body, loop_vars=[i, att_value, xpos])
