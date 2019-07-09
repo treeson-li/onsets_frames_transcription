2e6a484396b4b2c510c5c309b45459379c544109
treeson-li <treeson@gmail.com>
2019-07-09 16:37:19 +0800

    save_checkpoint_steps=10000

--- train_util.py
+++ train_util.py
@@ -71,8 +71,8 @@ def create_estimator(model_fn,
                      hparams,
                      use_tpu=False,
                      master='',
-                     save_checkpoint_steps=1000,
-                     save_summary_steps=1000,
+                     save_checkpoint_steps=10000,
+                     save_summary_steps=10000,
                      keep_checkpoint_max=None,
                      warm_start_from=None):
   """Creates an estimator."""
