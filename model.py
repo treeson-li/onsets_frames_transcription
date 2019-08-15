9512ff1bd774ecee9806390fa6a3997ec54cd911
treeson-li <treeson@gmail.com>
2019-08-15 14:56:51 +0800

    print shape spec_out_flat

--- model.py
+++ model.py
@@ -412,7 +412,11 @@ def model_fn(features, labels, mode, params, config):
       # spec_out_flat is not used during inference.
       if is_training:
         spec_out_flat = flatten_maybe_padded_sequences(spec_output, length)
+        print("-"*30)
+        print('spec_out_flat shape ', tf.shape(spspec_out_flatec))
         spec_out_flat = tf.reshape(spec_out_flat, (-1, spec_bins))
+        print('spec_out_flat reshape ', tf.shape(spspec_out_flatec))
+        print('spec_labels_flat shape ', tf.shape(spec_labels_flat))
         spec_labels_flat = flatten_maybe_padded_sequences(spec, length)
         spec_losses = tf_utils.log_loss(spec_labels_flat, spec_out_flat)
         tf.losses.add_loss(tf.reduce_mean(spec_losses))
